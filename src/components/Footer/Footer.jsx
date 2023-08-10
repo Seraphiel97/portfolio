a05ca5c0948a010024d0de102306b8ba7a9e060a
Seraphiel97 <seth.graves@outlook.com>
2023-08-10 12:38:50 -0400

    add header, footer, and tailwindcss

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h1>Contact Me</h1>
+        <h3>Email: seth.graves@outlook.com</h3>
+        <h3>Phone: 731-443-3232</h3>
+    </div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Seth Graves</h1>
+        <h3>LinkedIn</h3>
+        <h3>Github</h3>
+    </div>
+  )
+}
